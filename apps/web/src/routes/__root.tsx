5fcd01a90183b691f4f8dc6dd7d0c060afc6bf84
adamkruba <adamkruba6@gmail.com>
2026-05-26 16:50:26 +0000

    fix: import package

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -1,7 +1,7 @@
 import { QueryClientProvider } from "@tanstack/react-query"
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 import { createRootRoute, Link, Outlet } from "@tanstack/react-router"
-import { TanStackRouterDevtools } from "@tanstack/router-devtools"
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools"
 import { FileQuestion, Moon, Sun } from "lucide-react"
 import { useTranslation } from "react-i18next"
 import { ErrorBoundary } from "@/components/error"
